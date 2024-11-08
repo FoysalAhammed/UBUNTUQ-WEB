702b8821586af62589cbef4ca90eff92612d9d94
FoysalAhammed <hanif.mia2233@gmail.com>
2024-11-08 11:23:55 +0600

    initial commit

---
+++ components/SEO/Meta.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Foysal Ahammed Portfolio - Computer Engineering Student</title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Foysal Ahammed Portfolio - Computer Engineering Student" />
+            <meta name="description"
+                content="Foysal Ahammed's Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="Foysal Ahammed " />
+            <meta name="keywords"
+                content=", 's portfolio,  linux, ubuntu portfolio, Foysal Ahammed protfolio,Foysal Ahammed computer, Foysal Ahammed, Foysal ubuntu, Foysal Ahammed ubuntu portfolio" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/fevicon.png" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="Foysal Ahammed Portfolio - Computer Engineering Student" />
+            <meta itemProp="description"
+                content="Foysal Ahammed's Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/fevicon.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Foysal Ahammed Portfolio - Computer Engineering Student" />
+            <meta name="twitter:description"
+                content="Foysal Ahammed's  Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:creator" content="Foysal" />
+            <meta name="twitter:creator" content="Foysal" />
+            <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta name="og:title" content="Foysal Ahammed Portfolio - Computer Engineering Student" />
+            <meta name="og:description"
+                content="Foysal Ahammed's Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="og:image" content="images/logos/logo_1200.png" />
+            <meta name="og:url" content="https://foysal-portfolio.vercel.app" />
+            <meta name="og:site_name" content="Foysal Ahammed Personal Portfolio" />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+
+            <link rel="icon" href="images/logos/fevicon.svg"/>
+            <link rel="apple-touch-icon" href="images/logos/logo.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
